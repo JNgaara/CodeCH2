07148cf3300509c35960ae99865a6a486ecb0803
+ <joshngaara@gmail.com>
2025-01-08 11:22:09 +0200

    input js add btn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-let  shoppingList = [];
+let  shoppingList = document.getElementById('ShoppingList');
+shoppingList.addEventListener('submit', addItem);
+function addItem(e){
+    e.preventDefault();
+    let data = this.elements.ShoppingListInput.value;
+    let list = document.querySelector('ol');
+    let item = document.createElement('li');
+    let text = document.createElement('p');
+
+    text.textContent = data;
+    this.elements.ShoppingListInput.value= "";
+    item.append(text);
+    list.append(item);
+
+    let rmvBtn = document.createElement('span');
+    rmvBtn.classList.add('remove');
+    item.append(rmvBtn);
+    rmvBtn.addEventListener('click',deleteItem);
+
+}
+
+function deleteItem(e){
+    this.parentElement.remove();
+}
